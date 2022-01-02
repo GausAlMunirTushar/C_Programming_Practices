0b2874bfc5fe3b9ee3e16f96c7b1271b7b0cb580
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-01-02 20:49:36 +0600

    hello program run

--- program.c
+++ program.c
@@ -1,3 +1,11 @@
+/* program name:
+    * happy_new_year
+    *
+    * program description:
+    * This program run to happy new year
+    *
+    * program author:   Gaus Al Munir Tushar
+*/
 #include<stdio.h>
 int main(){
     printf("Happy New Year 2022");
