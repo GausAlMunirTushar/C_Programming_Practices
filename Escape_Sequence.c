cb710544ced0fdb9cf593dded27e8465804a5c5e
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-01-02 20:58:07 +0600

    c program comments and escape sequence

---
+++ Escape_Sequence.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("hello Escape Sequene\n"); // Using Escape Sequence
+    printf("Gaus Al Munir Tushar");
+    return 0;
+}
--- program.c
+++ program.c
@@ -1,9 +1,7 @@
 /* program name:
     * happy_new_year
-    *
     * program description:
     * This program run to happy new year
-    *
     * program author:   Gaus Al Munir Tushar
 */
 #include<stdio.h>
